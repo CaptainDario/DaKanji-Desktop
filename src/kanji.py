d3462ea8b9443bb052fd11e7c5e2ca38b70c62ef
dario <dario.klepoch@freenet.de>
2020-12-18 21:52:26 +0100

    generate random character for testing

--- src/kanji.py
+++ src/kanji.py
@@ -1,3 +1,5 @@
+import random
+
 import pyperclip
 
 from PySide6 import QtCore
@@ -7,12 +9,26 @@ class Kanji(QtCore.QObject):
     def __init__(self, character):
         QtCore.QObject.__init__(self)
         self.character = character
-        self.number = 12
 
-    @QtCore.Slot()
-    def out(self):
+    def generate_random_hanzi(self):
+        
+        random_word = ""
+
+        common, rare = range(0x4e00, 0xa000), range(0x3400, 0x4e00)
+        chars = list(map(chr, common))
+        random_word = chars[random.randint(0, len(chars))]
+
+        return random_word
+
+    @QtCore.Slot(result=str)
+    def copy_character(self):
+
+        self.character = self.generate_random_hanzi()
+
         pyperclip.copy(self.character)
         print(self.character)
+
+        return self.character
 
     def __str__(self):
         return 'Person "{}" {}'.format(self.character, self.number)
