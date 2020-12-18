00e3cf8ca27c030ebb250b3edb7455f55a3cf531
dario <dario.klepoch@freenet.de>
2020-12-18 21:45:36 +0100

    connect the ui with the python backend

--- src/main.py
+++ src/main.py
@@ -5,13 +5,26 @@ from PySide6.QtWidgets import QApplication
 from PySide6.QtQml import QQmlApplicationEngine
 
 from kanji import Kanji
+from canvas import Canvas
 
+kanjis = []
+nr_kanji = 10
+python_canvas = Canvas()
 
 if __name__ == "__main__":
 
     app = QApplication(sys.argv)
 
     engine = QQmlApplicationEngine()
+
+    ctx = engine.rootContext()
+    #set kanji object references
+    for i in range(nr_kanji):
+        kanjis.append(Kanji(""))
+        ctx.setContextProperty("kanji_" + str(i), kanjis[i])
+
+    #set the canvas object reference
+    ctx.setContextProperty("python_canvas", python_canvas)
 
     engine.load("main.qml")
 
