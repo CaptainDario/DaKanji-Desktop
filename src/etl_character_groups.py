771cc076ea7a5eb53bdac99fea23d87dbbd0a23d
dario <dario.klepoch@freenet.de>
2021-01-02 19:29:14 +0100

    Enum w. regex for filtering the data set by categ.

---
+++ src/etl_character_groups.py
@@ -0,0 +1,13 @@
+from enum import Enum
+
+class ETL_character_groups(Enum):
+
+    all      = r".*"
+
+    kanji    = r"[一-龯]"
+    katakana = r"[ァ-ン]"
+    hiragana = r"[ぁ-ん]"
+    number   = r"[0-9]"
+    roman    = r"[A-Za-z]"
+    #if it is none of the above, it has to be a symbol 
+    symbols  = r"^(?!" + "|".join([kanji, katakana, hiragana, number, roman]) + ")"
