c0bdc7bae0251019bc9c3b60be97bdd595b9c416
dario <dario.klepoch@freenet.de>
2021-01-02 19:27:54 +0100

    enum for the names of etl data set parts

---
+++ src/etl_data_names.py
@@ -0,0 +1,21 @@
+from enum import Enum
+
+class ETL_data_names(Enum):
+
+    ETL1  = "ETL1"
+    ETL2  = "ETL2"
+    ETL3  = "ETL3"
+    ETL4  = "ETL4"
+    ETL5  = "ETL5"
+    ETL6  = "ETL6"
+    ETL7  = "ETL7"
+    #provide the original naming...
+    ETL8B = "ETL8"
+    ETL8G = "ETL9"
+    ETL9B = "ETL10"
+    ETL9G = "ETL11"
+    #...and the renamed alternative
+    ETL8  = "ETL8"
+    ETL9  = "ETL9"
+    ETL10 = "ETL10"
+    ETL11 = "ETL11"
