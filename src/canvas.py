0e7522558fa666ddb01eab1c5acd54892d082cab
dario <dario.klepoch@freenet.de>
2021-01-02 19:20:10 +0100

    Added check that the image is not empty

--- src/canvas.py
+++ src/canvas.py
@@ -17,26 +17,26 @@ class Canvas(QtCore.QObject):
 
     @QtCore.Slot(str)
     def get_current_image(self, data_uri_image):
-        """[summary]
+        """ This method gets called everytime the user finished drawing a stroke on the canvas.
 
         Args:
-            data_uri_image ([type]): [description]
+            data_uri_image (str): The data_uri which contains the image from the QMLCanvas.
         """
         
         #convert the image from data_uri to PIL.Image
         img_base_64 = urllib.request.urlopen(data_uri_image)
         image = Image.open(img_base_64.file)
-        image = image.resize(size=(64, 64), resample=Image.ANTIALIAS, reducing_gap=2.0)
 
-        #convert image to np.array and make it grayscale
-        image = np.array(image)
-    
-        # 'convert' image to grayscale and normalize between (0, 1)
-        image = image[..., -1]
-        image[image > 50] = 255
-        image = image / image.max()
+        #check that image is not empty
+        if(image.getbbox()):
+            image = image.resize(size=(64, 64), resample=Image.ANTIALIAS, reducing_gap=2.0)
 
-        plt.imshow(image)
-        plt.show()
+            #convert image to np.array and make it grayscale
+            image = np.array(image)
+        
+            # 'convert' image to grayscale and normalize between (0, 1)
+            image = image[..., -1]
+            image[image > 50] = 255
+            image = image / image.max()
 
-        print("Image converted in python")
+            print("Image converted in python")
