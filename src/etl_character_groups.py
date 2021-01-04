449a4cee5cacf6b8eae635505b699cb4e9a2c580
dario <dario.klepoch@freenet.de>
2021-01-04 16:58:15 +0100

    added documentation for sphinx

--- src/etl_character_groups.py
+++ src/etl_character_groups.py
@@ -3,11 +3,22 @@ from enum import Enum
 class ETLCharacterGroups(Enum):
 
     all      = r".*"
+    """include everything"""
 
     kanji    = r"[一-龯]"
+    """kanji characters"""
+    
     katakana = r"[ァ-ン]"
+    """all katakana characters"""
+    
     hiragana = r"[ぁ-ん]"
+    """all hiragana characters"""
+    
     number   = r"[0-9]"
+    """numbers"""
+    
     roman    = r"[A-Za-z]"
-    #if it is none of the above, it has to be a symbol 
+    """roman characters (capital and not capital)"""
+
     symbols  = r"^(?!" + "|".join([kanji, katakana, hiragana, number, roman]) + ")"
+    """if it is none of the above, it has to be a symbol"""
