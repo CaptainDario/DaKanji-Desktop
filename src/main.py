d7876953b8dff15874e319056ba304f28a68d0ba
dario <dario.klepoch@freenet.de>
2021-01-05 20:57:36 +0100

    refactored to use ui not Canvas/PredictionButton

--- src/main.py
+++ src/main.py
@@ -4,29 +4,21 @@ import os
 from PySide6.QtWidgets import QApplication
 from PySide6.QtQml import QQmlApplicationEngine
 
-from prediction_button import PredictionButton
-from canvas import Canvas
+from ui import Ui
 
 
-predictionButtons = []
-nr_kanji = 10
-python_canvas = Canvas()
 
 if __name__ == "__main__":
 
+    # init app
     app = QApplication(sys.argv)
-
     engine = QQmlApplicationEngine()
-
-    ctx = engine.rootContext()
-    #set kanji object references
-    for i in range(nr_kanji):
-        predictionButtons.append(PredictionButton(""))
-        ctx.setContextProperty("predictionButton_" + str(i), predictionButtons[i])
-
-    #set the canvas object reference
-    ctx.setContextProperty("python_canvas", python_canvas)
-
+    
+    # connect the QML with python
+    ui = UI(engine.rootContext())
+    
+    # setup and load QML
     engine.load(os.path.join("ui", "main.qml"))
+
 
     sys.exit(app.exec_())
