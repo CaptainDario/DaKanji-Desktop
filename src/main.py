c4edf2ce2afc6b87b5c237f0dd5d6e4f3e96e199
dario <dario.klepoch@freenet.de>
2021-01-06 00:15:40 +0100

    set window icon

--- src/main.py
+++ src/main.py
@@ -2,6 +2,7 @@ import sys
 import os
 import hidden_import
 
+from PySide2.QtGui import QIcon
 from PySide2.QtWidgets import QApplication
 from PySide2.QtQml import QQmlApplicationEngine
 
@@ -14,12 +15,12 @@ if __name__ == "__main__":
     # init app
     app = QApplication(sys.argv)
     engine = QQmlApplicationEngine()
-    
-    # connect the QML with python
+
+    # connect QML and python
     ui = Ui(engine.rootContext())
     
     # setup and load QML
     engine.load(os.path.join("ui", "main.qml"))
-
+    app.setWindowIcon(QIcon("./icons/icon_eye_only.ico"))
 
     sys.exit(app.exec_())
