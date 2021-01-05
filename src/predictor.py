334113bfb4aac9d401a1d774cdfd9297bf9ed4ea
dario <dario.klepoch@freenet.de>
2021-01-05 21:06:18 +0100

    Function for predicting

--- src/predictor.py
+++ src/predictor.py
@@ -46,3 +46,34 @@ class Predictor():
         # get in-/output details
         self.input_details = self.kanji_interpreter.get_input_details()
         self.output_details = self.kanji_interpreter.get_output_details()
+
+
+    def predict(self, image : np.array, cnt_predictions : int) -> [str]:
+        """ Predict a character from an input image.
+
+        Args:
+            image      (np.array) : A numpy array with shape (1, 64, 64, 1) and dtype 'float32' 
+            cnt_predictions (int) : How many predictions should be returned ('cnt_predictions' most likely ones)
+
+        Returns:
+            A list with the 'cnt_predictions' most confident predictions.
+        """
+
+        # make prediction 
+        self.kanji_interpreter.set_tensor(self.input_details[0]["index"], image)
+        self.kanji_interpreter.invoke()
+        output_data = self.kanji_interpreter.get_tensor(self.output_details[0]["index"])
+        out_np = np.array(output_data)
+
+        # get the 'cnt_predictions' most confident predictions
+        preds = []
+        for i in range(cnt_predictions):
+            pred = self.label_binarizer.inverse_transform(out_np)
+            preds.append(pred[0])
+
+            #print("confidence:", out_np.max(), " --> ", pred)
+
+            # 'remove' this prediction from all
+            out_np[out_np.max() == out_np] = 0.0
+
+        return preds
