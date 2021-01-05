3dae486ff54ab29b6bc4d534f39d9932da1b5087
dario <dario.klepoch@freenet.de>
2021-01-05 23:18:51 +0100

    file with infos about the software

---
+++ src/about.py
@@ -0,0 +1,15 @@
+
+name           = "DaKanjiRecognizer"
+version        = "1.0"
+release_suffix = ""
+
+full_id = name + "_v" + version + release_suffix
+
+#Github
+repo_url           = "https://github.com/CaptainDario/DaKanjiRecognizer"
+pull_url           = "https://github.com/CaptainDario/DaKanjiRecognizer/pulls"
+latest_release_url = "https://github.com/CaptainDario/DaKanjiRecognizer/releases/latest"
+
+#Github API
+latest_release_api = "https://api.github.com/repos/CaptainDario/DaKanjiRecognizer/releases/latest"
+
