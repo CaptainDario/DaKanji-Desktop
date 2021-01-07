026029bf96eab951d689ff1caeae61f684b5d00a
CaptainDario <daapplab@gmail.com>
2021-01-08 00:13:42 +0100

    use pyside2's clipboard instead of ppyperclip

--- src/prediction_button.py
+++ src/prediction_button.py
@@ -1,21 +1,24 @@
-import pyperclip
 
 from PySide2 import QtCore
+from PySide2.QtGui import QClipboard
+
 
 class PredictionButton(QtCore.QObject):
     """ Python Class for connecting to QML PredictionButtons.
 
     Attributes:
-        _character (str) : The character which is currently being showed on the
-                            connected QML-button.
+        _character (        str) : The character which is currently being
+                                   showed on the connected QML-button.
+        clipboard   (QClipboard) : The OS's clipboard.
     """
 
     # signal which will be emitted when 'character' changed.
     character_changed = QtCore.Signal(str)
 
-    def __init__(self, character):
+    def __init__(self, clipboard : QClipboard):
         QtCore.QObject.__init__(self)
-        self._character = character
+        self._character = ""
+        self.clipboard = clipboard
     
 
     @QtCore.Property(str, notify=character_changed)
@@ -32,6 +35,6 @@ class PredictionButton(QtCore.QObject):
         """ Copies the button's character to clipboard
         """
 
-        pyperclip.copy(self.character)
+        self.clipboard.setText(self.character)
 
 
